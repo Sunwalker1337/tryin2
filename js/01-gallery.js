77b19f86dc4c285f6ea6c6ecd413972d787a44da
Oleksii Bukhanevych <holmivechnihpesen@gmail.com>
2022-08-11 15:39:22 +0300

    bubblin

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+let gallery = document.querySelector('.gallery')
+let imageGallery = galleryItems
+.map((element)=>
 
-console.log(galleryItems);
+`<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${element.preview}"
+      data-source="${element.original}"
+      alt="${element.description}"
+    />
+  </a>
+</div>`)
+
+gallery.insertAdjacentHTML("afterbegin", imageGallery.join(' '))
+
+
+gallery.addEventListener('click', zoomIn);
+
+function zoomIn () {
+    event.preventDefault()
+    if(event.target.nodeName === 'IMG'){
+        console.log('banana')
+    }
+}
